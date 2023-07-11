10f33eeff46f81daa622f1e95fdd0b0fc9a3febb
Щукин Павел Владимирович <p_shchukin@RTKIT.local>
2023-07-11 16:49:39 +0300

    made all tasks

---
+++ task001/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+void GetSecondNum(int num)
+{
+    if (num < 100 || num >= 1000)
+    {
+        Console.WriteLine("Ваше число не подходит");
+    }    
+    else
+    {
+    num = num / 10 % 10;
+    Console.WriteLine("Вторая цифра вашего числа: " + num);
+    }
+}
+
+Console.WriteLine("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+GetSecondNum(number);
+
---
+++ task002/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+void GetThirdDigit(int num)
+{
+    if (num < 100)
+    {
+        Console.WriteLine("В вашем числе нет третьей цифры");
+    }
+    else
+    {
+        while (num > 999)
+        {
+            num /=10; // Делим число на 10, пока оно больше 999
+        }
+        num = num % 10;        
+        Console.WriteLine("Третья цифра вашего числа: " + num);
+    }
+}
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+GetThirdDigit(number);
+
